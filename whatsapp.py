33f9d33a7170ff24ca17c9b206de345e271e5268
davidbalA592 <43343463+davidbalA592@users.noreply.github.com>
2021-05-17 10:08:38 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 # client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
-S_ID = "AC81301a8f1df45056589c4032856e2c58"
-auth = "82699285cc0f6690b48289b7472fa3f0"
+S_ID = <twilio S-id>
+auth = <twilio auth>
 client = Client(S_ID,auth)
 
 # this is the Twilio sandbox testing number
-from_whatsapp_number='whatsapp:+14155238886'
+from_whatsapp_number='whatsapp:<from_number>'
 # replace this number with your own WhatsApp Messaging number
-to_whatsapp_number='whatsapp:+919094458858'
+to_whatsapp_number='whatsapp:<to_number>'
 
 client.messages.create(body='Something went wrong: Unauthorised Access!',
                        from_=from_whatsapp_number,
